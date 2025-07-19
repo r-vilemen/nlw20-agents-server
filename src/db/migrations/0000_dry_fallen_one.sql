84f10c346d2c48fabe00191dcf122568b3f8408d
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2025-07-19 01:34:39 -0300

    feat: fix typo in rooms table and add questions table with foreign key constraint

--- src/db/migrations/0000_dry_fallen_one.sql
+++ src/db/migrations/0000_dry_fallen_one.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "rooms" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" text NOT NULL,
-	"descripition" text,
+	"description" text,
 	"created_at" timestamp DEFAULT now() NOT NULL
 );
